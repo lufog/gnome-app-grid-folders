a61541a551bb7a6943fb9bf02e88f14742841fa3
Aleksey Smirnov <debugger94@gmail.com>
2024-11-03 20:55:15 +0300

    Added check for existence of folder-list file and basic check for its correctness

--- create-folders.sh
+++ create-folders.sh
@@ -2,11 +2,21 @@
 GS_SCHEMA=org.gnome.desktop.app-folders
 GS_PATH=/org/gnome/desktop/app-folders/folders
 
+if [ ! -f folder-list ]; then
+    echo "folder-list not found. This file is required because it contains the names and categories of the folders to be created."
+    exit 1
+fi
+
 folder_path=()
 folder_name=()
 folder_categories=()
 
 source ./folder-list
+
+if [[ ${#folder_path[@]} != ${#folder_name[@]} || ${#folder_path[@]} != ${#folder_categories[@]} ]]; then
+   echo "folder-list has an invalid structure. Perhaps in some field you used = instead of +=."
+   exit 1
+fi
 
 # Helpers
 arr_to_fmt_str() {
